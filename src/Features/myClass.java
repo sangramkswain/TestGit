9eb0477fd18e53283aa8b93483c9f0887c2c37ea
Sangram Keshari Swain - ERS <Sangram.Swain@hcl.com>
2017-12-28 09:24:44 +0530

    Again checking

---
+++ src/Features/myClass.java
@@ -0,0 +1,16 @@
+package Features;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class myClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sangram");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.myhcl.com/Login/home.aspx");
+
+	}
+
+}
